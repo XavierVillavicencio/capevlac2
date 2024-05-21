47ae85c47abd1c0c9c7ad35f231814a69f68afe6
Xavier Villavicencio B <xavier.villavicencio.b@gmail.com>
2024-05-21 17:44:58 +0000

    feat: update multisitio

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -83,6 +83,14 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+define( 'WP_ALLOW_MULTISITE', true );
+
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+define( 'DOMAIN_CURRENT_SITE', 'cap.olade.org' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 
 
 /* That's all, stop editing! Happy publishing. */
